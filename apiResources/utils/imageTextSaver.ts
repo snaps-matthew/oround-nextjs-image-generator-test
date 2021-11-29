4809cd6eb1089cf36a8a019bc085a3b3b2f84967
david <david@snaps.com>
2021-11-29 13:56:04 +0900

    파일 쓰기에 await 걸기

--- apiResources/utils/imageTextSaver.ts
+++ apiResources/utils/imageTextSaver.ts
@@ -4,5 +4,5 @@ export const imageTextSaver = async (imgData:any, target:string) => {
   const base64 = 'data:image/png;base64';
   const conversion = imgData.split(',')[0] === base64 ? imgData : `data:image/png;base64,${imgData}`;
 
-  fs.writeFileSync(`apiResources/resources/${target}.txt`, conversion);
+  await fs.writeFileSync(`apiResources/resources/${target}.txt`, conversion);
 }
