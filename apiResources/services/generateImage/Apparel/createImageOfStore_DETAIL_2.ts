336dfaba7434bac5be2beda9988e91437104d67e
david <david@snaps.com>
2021-12-14 19:41:36 +0900

    이미지 생성 지우기

--- apiResources/services/generateImage/Apparel/createImageOfStore_DETAIL_2.ts
+++ apiResources/services/generateImage/Apparel/createImageOfStore_DETAIL_2.ts
@@ -85,8 +85,6 @@ export const createImageOfStore_DETAIL_2 = async (props:any) => {
   // (2) 주름 생성하기
   const artworkWrinkled:any = await getImageWrinkled(productPath, productCode, patternImageFileName);
   const artworkImage = await loadImage(`data:image/png;base64,${artworkWrinkled}`);
-  console.log(['PROD CODE'], productCode);
-  await imageTextSaver(`data:image/png;base64,${artworkWrinkled}`, 'apiResources/resources/ASDFJA')
   ctx.globalCompositeOperation = 'source-over';
   ctx.drawImage(artworkImage, 0, 0);
   // await applyInnerWrinkle(canvas, tempCanvas, productCropImage);
