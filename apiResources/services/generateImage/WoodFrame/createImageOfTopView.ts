8c57720e52ad7c14b45041c650abc6bfb68f7c4a
joel <joel@snaps.com>
2021-12-17 20:47:47 +0900

    fix wood frame

--- apiResources/services/generateImage/WoodFrame/createImageOfTopView.ts
+++ apiResources/services/generateImage/WoodFrame/createImageOfTopView.ts
@@ -36,7 +36,7 @@ export const createImageOfTopView = async (props:{templateImage: any, productEdi
   let ratio = 0
   if(productEditInfo.size.length > 0){
     ratio = productEditInfo.size[0].horizontalSizePx / productEditInfo.size[0].horizontalSizeMm;
-    ratio=ratio*scale
+    // ratio=ratio*scale
   }else{
     //사이즈가 없는경우 더미이미지로 리턴
     const dummyOroundImage = await loadErrorImage("size empty")
