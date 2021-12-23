53597510fc6d4d65c37a8f05fa49a159e557865e
david <david@snaps.com>
2021-12-23 18:03:01 +0900

    feat event product thumbnail image output resized to 1000 by 1000

--- apiResources/services/generateImage/EventProduct/EventProduct.ts
+++ apiResources/services/generateImage/EventProduct/EventProduct.ts
@@ -3,15 +3,23 @@ import TargetType from 'apiResources/constants/TargetType';
 import { Canvas, createCanvas } from 'canvas';
 import { loadImage } from 'apiResources/utils/loadImage';
 import { newCanvas } from 'apiResources/utils/newCanvas';
+import { getArtworkImage } from '../../../utils/getSelectedScene';
+import { imageFull } from '../../../utils/imageAlign';
 
 class EventProduct {
   protected productResourcePath: string;
   protected canvas: Canvas;
   protected target: string;
+  protected  artworkImage: Canvas;
+  protected  artworkImageWidth: number;
+  protected  artworkImageHeight: number;
   protected ext: string;
 
   constructor() {
     this.productResourcePath = '';
+    this.artworkImage = createCanvas(1000, 1000);
+    this.artworkImageWidth = 0;
+    this.artworkImageHeight = 0;
     this.target = '';
     this.ext = '';
     this.canvas = createCanvas(1000, 1000);
@@ -31,16 +39,17 @@ class EventProduct {
 
     } else if (data.target === TargetType.STORE_DETAIL_2) {
 
-      this.productResourcePath = productCode === '1040190002' ? `${Config.RESOURCE_CDN_URL}/${productCode}/${artProductIndex}_list1.png` : `${Config.RESOURCE_CDN_URL}/${productCode}/${artProductIndex}_view.png`;
+      this.productResourcePath = `${Config.RESOURCE_CDN_URL}/${productCode}/${artProductIndex}_view.png`;
 
     } else if (data.target === TargetType.STORE_DETAIL_3) {
 
-      this.productResourcePath = `${Config.RESOURCE_CDN_URL}/${productCode}/${artProductIndex}_list.png`;
+      this.productResourcePath = productCode === '1040190002' ? `${Config.RESOURCE_CDN_URL}/${productCode}/${artProductIndex}_list1.png` : `${Config.RESOURCE_CDN_URL}/${productCode}/${artProductIndex}_list.png`;
 
     } else {
-
-      this.canvas = thumbnailImage;
-
+      const {artworkImage, artworkImageWidth, artworkImageHeight}  = await getArtworkImage(productEditInfo, optionInfo);
+      this.artworkImage = artworkImage;
+      this.artworkImageWidth = artworkImageWidth;
+      this.artworkImageHeight = artworkImageHeight;
     }
   }
 
@@ -49,6 +58,10 @@ class EventProduct {
     if (this.target !== TargetType.STORE_DETAIL_4) {
       const loadedImage = await loadImage(this.productResourcePath);
       ctx.drawImage(loadedImage, 0, 0);
+    } else {
+
+      const size = imageFull(this.artworkImageWidth, this.artworkImageHeight, 1000, 1000, 0);
+      ctx.drawImage(this.artworkImage, size.x, size.y, size.width, size.height);
     }
   }
 
